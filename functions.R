e27cc8519a6dd8fd89b3eaeeb54fd40e85a44b55
julietakaminsky <kaminsky.julieta@gmail.com>
2018-08-17 15:18:57 -0300

    Rmd and script

--- functions.R
+++ functions.R
@@ -1,3 +1,21 @@
 # Functions definitions
 
-# 
+#' Title
+#'
+#' @param parameters 
+#'
+#' @return
+#' @export
+#'
+#'Data accessibility
+#'
+#' @examples
+funName <- function(parameters) {
+
+}
+
+# loops
+
+for( i in 1:100) {
+  
+}
